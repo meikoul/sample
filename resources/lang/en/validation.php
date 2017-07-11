83e26cde6485dee5b45f641b06966ddabf27c7f0
vagrant <vagrant@homestead>
2017-07-11 03:50:05 +0000

    用户注册

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -91,6 +91,16 @@ return [
     'custom' => [
         'attribute-name' => [
             'rule-name' => 'custom-message',
+
+        ],
+        'email' => [
+            'required' => '邮箱地址不能为空！',
+        ],
+        'name' => [
+            'required' => '名称不能为空！',
+        ],
+        'password' => [
+            'required' => '密码不能为空！',
         ],
     ],
 
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,15 +1,17 @@
-<!DOCTYPE html>
 <html>
-  <head>
-    <title>@yield('title', 'Sample App')</title>
-    <link rel="stylesheet" href="/css/app.css">
-  </head>
-  <body>
-     @include('layouts._header')
+<head>
+  <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+@include('layouts._header')
 
-    <div class="container">
-      @yield('content')
-       @include('layouts._footer')
-    </div>
-  </body>
+<div class="container">
+  <div class="col-md-offset-1 col-md-10">
+    @include('shared.messages')
+    @yield('content')
+    @include('layouts._footer')
+  </div>
+</div>
+</body>
 </html>
